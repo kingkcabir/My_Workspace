a8ef7a06328b543e927ccf731c55d24a3ca962ac
ABDULLAH KABIR <146261294+kingkcabir@users.noreply.github.com>
2025-01-16 02:50:55 -0800

    Add files via upload

--- vowel.py
+++ vowel.py
@@ -1,4 +1,17 @@
-text = input("input your words")
+text = input("input your words: ")
+text = text.lower()
+vowels = []
+consonant = []
 for ch in text:
-    if ch in 'AaEeIiOoUu':
-        print("vowel_count:", ch, end=" ,")
+    if ch in 'aeiou':
+        vowels.append(ch)
+    if ch in 'bcdfghjklmnpqrstvwxyz':
+        consonant.append(ch)
+    
+print('vowels or consonants?')
+answer = input('your response: ')
+if answer == 'vowels':
+    print('vowels in word are: ', ' '.join(vowels), end=' ')
+        
+elif answer == 'consonant':
+    print('consonant in word are: ', ' '.join(consonant), end=' ')
